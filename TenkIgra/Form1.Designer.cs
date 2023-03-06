499124d04a3717f7c6a88cc1fa7606942bf4292e
Alexandra Ljustina <acoljustina2004@gmail.com>
2023-03-06 12:15:40 +0100

    Final push
    The project is finalised and is functional

---
+++ TenkIgra/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace TenkIgra
+{
+    partial class TankGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Tenk = new System.Windows.Forms.PictureBox();
+            this.Score = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Tenk)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Tenk
+            // 
+            this.Tenk.Image = global::TenkIgra.Properties.Resources.tigar_2;
+            this.Tenk.Location = new System.Drawing.Point(321, 629);
+            this.Tenk.Name = "Tenk";
+            this.Tenk.Size = new System.Drawing.Size(61, 120);
+            this.Tenk.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Tenk.TabIndex = 0;
+            this.Tenk.TabStop = false;
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Font = new System.Drawing.Font("Old English Text MT", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Score.Location = new System.Drawing.Point(12, 720);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(118, 32);
+            this.Score.TabIndex = 1;
+            this.Score.Text = "Score: 0";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Interval = 20;
+            // 
+            // TankGame
+            // 
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(734, 761);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.Tenk);
+            this.Name = "TankGame";
+            this.Text = "Tank Game";
+            ((System.ComponentModel.ISupportInitialize)(this.Tenk)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Tenk;
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ TenkIgra/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TenkIgra
+{
+    public partial class TankGame : Form
+    {
+        bool levo, desno;
+        int brzinaTenka = 12;
+        int brzinaMete = 5;
+        int score = 0;
+        int projektilMeteTimer = 300;
+
+        PictureBox[] shermans;
+
+        bool shooting;
+        bool isGameOver;
+
+
+        public TankGame()
+        {
+            InitializeComponent();
+            gameSetUp();
+        }
+
+        private void mainGameTimeEvent(object sender, EventArgs e)
+        {
+            Score.Text = "SCORE: " + score;
+
+            if (levo)
+            {
+                Tenk.Left -= brzinaTenka;
+            }
+            if (desno)
+            {
+                Tenk.Left += brzinaTenka;
+            }
+
+            projektilMeteTimer -= 10;
+
+            if(projektilMeteTimer < 1)
+            {
+                projektilMeteTimer = 300;
+                projektil("shermanShell");
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "shermans")
+                {
+                    x.Left += brzinaMete;
+
+                    if(x.Left > 730)
+                    {
+                        x.Top += 90;
+                        x.Left = -80;
+                    }
+
+                    if(x.Bounds.IntersectsWith(Tenk.Bounds))
+                    {
+                        gameOver("Scheiße! amerikanische Schweine haben Ihre Position erreicht!");
+                    }
+
+                    foreach(Control y in this.Controls)
+                    {
+                        if(y is PictureBox && (string)y.Tag == "shell")
+                        {
+                            if (y.Bounds.IntersectsWith(x.Bounds))
+                            {
+                                this.Controls.Remove(x);
+                                this.Controls.Remove(y);
+                                score += 1;
+                                shooting = false;
+                            }
+                        }
+                    }
+                }
+
+                if(x is PictureBox && (string)x.Tag == "shell")
+                {
+                    x.Top -= 20;
+
+                    if(x.Top < 15)
+                    {
+                        this.Controls.Remove(x);
+                        shooting = false;
+                    }
+
+                }
+
+                if (x is PictureBox && (string)x.Tag == "shermanShell")
+                {
+                    x.Top += 20;
+
+                    if(x.Top > 600)
+                    {
+                        this.Controls.Remove(x);
+                    }
+
+                    if (x.Bounds.IntersectsWith(Tenk.Bounds))
+                    {
+                        this.Controls.Remove(x);
+                        gameOver("Scheiße! Amerikaner haben Ihren Panzer zerstört!");
+                    }
+                }
+            }
+
+            if(score > 10)
+            {
+                brzinaMete = 8;
+            }
+
+            if (score > 15)
+            {
+                brzinaMete = 10;
+            }
+
+            if (score == shermans.Length)
+            {
+                gameOver("Du hast alle Amerikaner besiegt!");
+            }
+        }
+
+        private void strelicaDolje(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                levo = true;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                desno = true;
+            }
+        }
+
+        private void strelicaGore(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                levo = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                desno = false;
+            }
+            if(e.KeyCode == Keys.Space && shooting == false)
+            {
+                shooting = true;
+                projektil("shell");
+            }
+            if (e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                removeAll();
+                gameSetUp();
+            }
+        }
+
+        private void makeShermans()
+        {
+            shermans = new PictureBox[20];
+
+            int left = 0;
+
+            for(int i = 0; i < shermans.Length; i++)
+            {
+                shermans[i] = new PictureBox();
+                shermans[i].Size = new Size(35, 70);
+                shermans[i].Image = Properties.Resources.sherman;
+                shermans[i].Top = 5;
+                shermans[i].Tag = "shermans";
+                shermans[i].Left = left;
+                shermans[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                this.Controls.Add(shermans[i]);
+                left = left - 80;
+            }
+        }
+
+        private void gameSetUp()
+        {
+            Score.Text = "SCORE: 0";
+            score = 0;
+            isGameOver = false;
+
+            projektilMeteTimer = 300;
+            brzinaMete = 5;
+            shooting = false;
+
+            makeShermans();
+            GameTimer.Start();
+        }
+
+        private void gameOver(string poruka)
+        {
+            isGameOver = true;
+            GameTimer.Stop();
+            Score.Text = "SCORE: " + score + " " + poruka;
+        }
+
+        private void removeAll()
+        {
+            foreach(PictureBox i in shermans)
+            {
+                this.Controls.Remove(i);
+            }
+
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox)
+                {
+                    if((string)x.Tag == "shell" || (string)x.Tag == "shermanShell")
+                    {
+                        this.Controls.Remove(x);
+                    }
+                }
+            }
+        }
+
+        private void projektil(string projektilTag)
+        {
+            PictureBox shell = new PictureBox();
+            shell.Image = Properties.Resources.shell;
+            shell.Size = new Size(5, 20);
+            shell.SizeMode = PictureBoxSizeMode.StretchImage;
+            shell.Tag = projektilTag;
+            shell.Left = Tenk.Left + Tenk.Width / 2;
+
+            if((string)shell.Tag == "shell")
+            {
+                shell.Top = Tenk.Top - 20;
+            }
+            else if ((string)shell.Tag == "shermanShell")
+            {
+                shell.Top = -100;
+            }
+
+            this.Controls.Add(shell);
+            shell.BringToFront();
+        }
+    }
+}
